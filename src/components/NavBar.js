665496eb98173cfa7d3d4af609bc9ea5aadbfe24
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2017-12-19 02:19:13 +0700

    :sparkles: Add NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import FontAwesome from 'react-fontawesome';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  background-color: white;
+  box-shadow: -1px -1px 3px gray;
+  padding: 0 !important;
+`
+const NavItem = styled.span`
+  cursor: pointer;
+  color: black;
+  padding: 12px;
+  margin: 0;
+  font-size: 18px;
+  transition: background 200ms;
+
+  &:hover {
+    background-color: #f2f2f2;
+  }
+`
+const NavBar = () => (
+  <Nav className="navbar navbar-light navbar-expand-lg fixed-top">
+    <div className="container">
+      <a className="navbar-brand" href="/">
+        <FontAwesome name="code" size="lg" />
+        <b className="mx-2">
+          Kongpon
+        </b>
+        <FontAwesome name="copyright" size="lg" style={{color: 'orange'}} />
+      </a>
+      <div className="navbar-nav">
+        <NavItem className="nav-item">
+          <FontAwesome name='home' /> Home
+        </NavItem>
+        <NavItem className="nav-item">
+          <FontAwesome name='user-circle-o' /> About
+        </NavItem>
+        <NavItem className="nav-item">
+          <FontAwesome name='star' /> Achievements
+        </NavItem>
+        <NavItem className="nav-item">
+          <FontAwesome name='th' /> Projects
+        </NavItem>
+        <NavItem className="nav-item">
+          <FontAwesome name='send' /> Contact
+        </NavItem>
+      </div>
+    </div>
+  </Nav>
+)
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  * {
+    font-family: 'work sans';
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
