ddf102ded40f8d89324b726ad516d35a38bd3e8a
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-01-01 22:47:08 +0700

    :sparkles: Add scroll to contact

--- src/data/profile.js
+++ src/data/profile.js
@@ -1,15 +1,22 @@
 import React, { Fragment } from 'react'
+import { Link } from 'react-scroll'
+
 const profile = {
   name: 'Kongpon Charanwattanakit',
   position: 'Software Developer',
   info: (
     <Fragment>
-      Hi, my name is <b>Jacky</b>.
+      Hi, my name is <b><u>Jacky</u></b>.
       I am currently a Software Engineering student at Kasetsart University.
       I was fascinated by the art of programming,
       which brought me into this challenging field.
       I'm also looking for an internship in a software development company.
-      If you are interested in me, feel free to <a href="/">contact</a>!
+      If you are interested in me, feel free to{' '}
+      <a href="#contact">
+        <Link smooth="easeInOutQuint" to="contact">
+          <b>contact!</b>
+        </Link>
+      </a>
     </Fragment>
   )
 }
